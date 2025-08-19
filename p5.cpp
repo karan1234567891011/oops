a6b398023cb16433f9f6ab79366c50ec0290f12f
karan1234567891011 <karanMaitRR@gmail.com>
2025-08-19 14:38:42 +0530

    Update p5.cpp

--- p5.cpp
+++ p5.cpp
@@ -1,9 +1,9 @@
-// Online C++ compiler to run C++ program online
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    // Write C++ code here
+    
     int a ;
     int fac=1;
     int i = 1;
@@ -17,4 +17,5 @@ int main() {
     cout<<fac;
 
     return 0;
+
 }
